f82a6026677fd307b05f7b3d48cb25e2fd76c459
Donne Martin <donne.martin@gmail.com>
2015-12-20 07:04:10 -0500

    Only support Python 3.
    In the future, consider adding support for Python 2.

--- setup.py
+++ setup.py
@@ -35,8 +35,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
