b3bac0395f23901d5338d4c036076307ce8c73b1
Donne Martin <donne.martin@gmail.com>
2015-12-28 08:19:48 -0500

    Update AwesomeCli docstrings.

--- awesome/awesome_cli.py
+++ awesome/awesome_cli.py
@@ -23,7 +23,7 @@ class AwesomeCli(object):
         """Main entry point for AwesomeCli.
 
         :type ctx: :class:`click.core.Context`
-        :param ctx: stores an instance of Awesome used to update the README.
+        :param ctx: Stores an instance of Awesome used to update the README.
         """
         # Create a GitHub object and remember it as as the context object.
         # From this point onwards other commands can refer to it by using the
@@ -35,6 +35,9 @@ class AwesomeCli(object):
     @pass_awesome
     def rock_it(awesome, readme_path):
         """Updates the README.
+
+        :type awesome: :class:`Awesome`
+        :param awesome: Stores an instance of Awesome used to update the README.
 
         :type readme_path: str
         :param readme_path: The README path
