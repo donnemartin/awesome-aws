efac622ea218d47435acf71e2498c7680d05271d
Donne Martin <donne.martin@gmail.com>
2019-08-14 07:39:52 -0400

    Update Python versions

--- setup.py
+++ setup.py
@@ -38,6 +38,8 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
